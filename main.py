5c78e660c8f6555f45d5f0cdbb3ca80784deea2d
nishant152030 <127687512+nishant152030@users.noreply.github.com>
2024-06-21 00:34:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
+import numpy as np 
+import pandas as pd 
 import os
 import tensorflow as tf
 import keras.preprocessing.image as process_im
